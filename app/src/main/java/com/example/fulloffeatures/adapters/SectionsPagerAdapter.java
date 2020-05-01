f8b9d2e40363caabe63b43bad0ef4ef9de8a9928
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2020-05-01 12:43:38 +0430

    tab title string array in resources

--- app/src/main/java/com/example/fulloffeatures/adapters/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/fulloffeatures/adapters/SectionsPagerAdapter.java
@@ -3,7 +3,6 @@ package com.example.fulloffeatures.adapters;
 import android.content.Context;
 
 import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -19,13 +18,13 @@ import com.example.fulloffeatures.fragments.ShakeItFragment;
  */
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-    @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
     private final Context mContext;
+    private String[] tabTitles;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
         mContext = context;
+        tabTitles = context.getResources().getStringArray(R.array.tab_title);
     }
 
     @Override
@@ -43,7 +42,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        return mContext.getResources().getString(TAB_TITLES[position]);
+        return tabTitles[position];
     }
 
     @Override
