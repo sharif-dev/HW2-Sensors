4042932bcc294726137e428d490736283e9b3f80
mhbahmani <mhbahmani79@gmail.com>
2020-05-05 19:32:35 +0430

    add TurnService

---
+++ app/src/main/java/com/example/fulloffeatures/services/TurnService.java
@@ -0,0 +1,83 @@
+package com.example.fulloffeatures.services;
+
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.example.fulloffeatures.MainActivity;
+import com.example.fulloffeatures.R;
+import com.example.fulloffeatures.sensors.ShakeSensor;
+import com.example.fulloffeatures.sensors.TurnSensor;
+
+public class TurnService extends Service {
+
+    public static final String CHANNEL_ID = "ForegroundServiceChannel";
+    TurnSensor sensor;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sensor = new TurnSensor(this);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Foreground Service")
+                .setContentText(getString(R.string.shake_listen))
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1, notification);
+        //do heavy work on a background thread
+        //stopSelf();
+
+        sensor.setSensitivity((int) intent.getExtras().get(getString(R.string.sensitivity)));
+        sensor.startListening();
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        sensor.stopListening();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
+    }
+
+}
