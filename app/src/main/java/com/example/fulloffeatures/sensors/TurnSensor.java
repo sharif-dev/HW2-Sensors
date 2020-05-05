9507a15dc1105f8ea8e1b9466be8ba2e4f667d5f
mhbahmani <mhbahmani79@gmail.com>
2020-05-05 22:54:21 +0430

    add turnoff method to turn sensor class

--- app/src/main/java/com/example/fulloffeatures/sensors/TurnSensor.java
+++ app/src/main/java/com/example/fulloffeatures/sensors/TurnSensor.java
@@ -1,12 +1,14 @@
 package com.example.fulloffeatures.sensors;
 
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.widget.Toast;
+import android.os.PowerManager;
+import android.util.Log;
 
 
 import static android.content.Context.SENSOR_SERVICE;
@@ -18,6 +20,8 @@ public class TurnSensor implements SensorEventListener {
     Context context;
     private long lastUpdate;
     private static int TOLERANCE = 4;
+
+    private final String TAG = "sensor";
 
     public TurnSensor(Context context) {
         this.context = context;
@@ -37,9 +41,19 @@ public class TurnSensor implements SensorEventListener {
             double z = values[SensorManager.DATA_Z];
 
             if (y > -TOLERANCE && y < TOLERANCE && z > 8 - TOLERANCE && z < 8 + TOLERANCE) {
-                Toast.makeText(context, "turning phone off", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "turning screen off");
+                turnoff();
             }
         }
+    }
+
+    private void turnoff() {
+        PowerManager manager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        assert manager != null;
+        @SuppressLint("InvalidWakeLockTag") PowerManager.WakeLock wl =
+                manager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+        wl.acquire(10 * 60 * 1000L /*10 minutes*/);
+        wl.release();
     }
 
     @Override
@@ -54,13 +68,5 @@ public class TurnSensor implements SensorEventListener {
 
     public void startListening() {
         sensorManager.registerListener(this, acceleratorSensor, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    public void setSensitivity(int sensitivity) {
-        TOLERANCE = convert(sensitivity);
-    }
-
-    private int convert(int percent) {
-        return 35 - percent / 5;
     }
 }
